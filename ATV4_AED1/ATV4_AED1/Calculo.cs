02418cc12d86f3c7a1269b81977202769f8e1fa2
Vitor Lorenzo <81115033+VitorLorenzo@users.noreply.github.com>
2023-03-15 19:54:10 -0300

    Add files via upload
    Teste em POO 2

---
+++ ATV4_AED1/ATV4_AED1/Calculo.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    internal class Calculo
+    {
+        private int x;
+        private double y;
+        private double z;
+        private double rx;
+        private double ry;
+        private double rz;
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+          
+        }
+        public double Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+        public double Z
+        {
+            get
+            {
+                return z;
+            }
+            set
+            {
+                z = value;  
+            }
+        }
+        public double RX
+        {
+            get
+            {
+                return rx;
+            }
+            set
+            {
+                rx = value;
+            }
+
+        }
+        public double RY
+        {
+            get
+            {
+                return ry;
+            }
+            set
+            {
+                ry = value;
+            }
+        }
+        public double RZ
+        {
+            get
+            {
+                return rz;
+            }
+            set
+            {
+                rz = value;
+            }
+        }
+
+
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Primeiro lado: " + rx);
+            Console.WriteLine();
+            Console.WriteLine("segundo lado: " + ry);
+            Console.WriteLine();
+            Console.WriteLine("terceiro lado:" + rz);
+            Console.WriteLine();
+
+        }
+        
+    }
+}
---
+++ ATV4_AED1/ATV4_AED1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+
+namespace Triangulo
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculo t = new Calculo();
+
+            Console.WriteLine("Digite o valor do primeiro lada: ");
+            t.X = int.Parse(Console.ReadLine());
+            t.RX = Math.Pow(t.X,2);
+            Console.WriteLine(t.RX);
+            Console.WriteLine("Digite o valor do segundo lado: ");
+            t.Y = double.Parse(Console.ReadLine());
+            t.RY = Math.Pow(t.Y,2);
+            Console.WriteLine(t.RY);
+            Console.WriteLine("Digite o valor do terceiro lado: ");
+            t.Z = double.Parse(Console.ReadLine());
+            t.RZ = Math.Pow(t.Z,2);
+            Console.WriteLine(t.RZ);
+
+            if (t.RX >= t.RY + t.RZ)
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            }
+            if(t.RX == t.RY + t.RZ)
+            {
+                Console.WriteLine("É UM TRIANGULO RETANGULO");
+            }
+            if(t.RX > t.RY + t.RZ)
+            {
+                Console.WriteLine("É UM TRIANGULO OBTUSANGULO");
+            }
+            if(t.RX < t.RY + t.RZ)
+            {
+                Console.WriteLine("É UM TRIANGULO ACUTANGULO");
+            }
+            if (t.RX == t.RY && t.RX != t.RZ)
+            {
+                Console.WriteLine("É UM TRIANGULO ISOSCELES");
+            }
+            if (t.RX == t.RY || t.RX == t.RZ || t.RY == t.RZ)
+            {
+                Console.WriteLine("É UM TRIANGULO EQUILATERO");
+            }
+            else
+            {
+              Console.WriteLine("Digite outros valores por favor");
+            }
+
+
+
+            t.ImprimirDados();
+
+
+          
+
+
+           
+
+
+        }
+    }
+}//double.Parse(Console.ReadLine("F4", CultureInfo.InvariantCulture))
